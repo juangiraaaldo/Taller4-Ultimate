2cceb77b4e656e1f7ea32fb8de75dda33b9d922d
juangiraaaldo <juangiraaaldo10@gmail.com>
2026-04-17 10:11:58 -0500

    github o vercel link

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Taller4-Ultimate/',
+  base: process.env.NODE_ENV === 'production' && !process.env.VERCEL
+  ? '/Taller4-Ultimate/'
+  : '/',
+
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'GamerZone Ultimate',
+        short_name: 'GamerZone',
+        description: 'Mi tienda de libros y juegos',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+
   build: {
     rollupOptions: {
       output: {
