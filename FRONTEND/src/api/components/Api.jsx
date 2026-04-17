e12c2eb7f4141ee679029bb6919201cf2e7131b0
juangiraaaldo <juangiraaaldo10@gmail.com>
2026-04-17 12:05:37 -0500

    cambio name api forzado

--- FRONTEND/src/api/components/Api.jsx
+++ FRONTEND/src/api/components/Api.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { getPersonajes } from '../services/mierdadque'
+import { getPersonajes } from '../services/rickandmortyservice'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
