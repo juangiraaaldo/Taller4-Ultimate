5e8a5122b9bfe43f81cb5b9491be63eaa13eb0ed
juangiraaaldo <juangiraaaldo10@gmail.com>
2026-04-17 13:57:36 -0500

    da

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -14,5 +14,9 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes); 
 
+app.get('/test', (req, res) => {
+  res.send('FUNCIONA');
+});
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(` El servidor esta corriendo en el puerto ${PORT}`));
